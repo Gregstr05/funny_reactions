964f6503ae6be1ec7ed93ff73ef7d05e556f8be3
Adam Gregořica <44508744+Gregstr05@users.noreply.github.com>
2022-01-18 16:38:09 +0100

    updated index.py
    Added timestamps to console output

--- index.py
+++ index.py
@@ -5,43 +5,53 @@ import asyncio
 import json
 import sys, datetime, time
 
+
+
 with open('config.json') as f:
     token = json.load(f)
 type(token)
 print(token.keys())
+
+with open('uwu.json') as i:
+    uwu = json.load(i)
+type(uwu)
+print(uwu.keys())
+
 
 bot = commands.Bot(command_prefix='$')
  
 @bot.event
 async def on_ready():
     print ("I am alive")
- 
- 
+    print ()
+
 @bot.event
 async def on_message(message):
-    print ("New message")
+    now = datetime.datetime.now()
+    time = (now.strftime("%Y-%m-%d %H:%M:%S"))
+    print (time, " New message")
     emoji = bot.get_emoji("ehm:929430156907540491")
     #await message.add_reaction("👀")
     if(message.author.id == "577161725284057138"):
-        print ("fugnuju skoro")
+        print (time, " fugnuju skoro")
         await message.add_reaction(emoji)
     if '420' in message.content:
-        print('funny guy')
+        print(time, ' funny guy')
         await message.add_reaction("🥬")
     if '69' in message.content:
-        print('funny guy')
+        print(time, ' funny guy')
         await message.add_reaction("💦")
     if 'agrLULE' in message.content:
-        print('funny guy')
+        print(time, ' funny guy')
         await message.add_reaction("agrLULE:914561735040589864")
     if 'uwu' in message.content:
-        print('Weeb')
+        print(time, ' Weeb')
         channel = bot.get_channel(message.channel.id)
         await message.add_reaction("Pepenoweeb:892154131228278846")
         #await channel.message.send("Pepenoweeb:892154131228278846")
 
     if 'https://tenor.com/view/bye-felicia-hi-hello-bye-gif-14084675' in message.content:
-        print('Dobrou noc')
+        print(time, ' Dobrou noc')
         await message.add_reaction("agrLove:917046239202246656")
    
  
